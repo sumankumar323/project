6f92173500a40f671ba78a787b4795ce885719f0
SAKET KUMAR <saketk89@gmail.com>
2022-06-16 15:40:35 +0530

    completed auth 2

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -21,6 +21,7 @@ const authorise = function(req, res, next) {
     let token = req.headers["x-auth-token"]
     if(!token) return res.send({status: false, msg: "token must be present in the request header"})
     let decodedToken = jwt.verify(token, 'functionup-radon')
+    console.log(decodedToken)
 
     if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
     
