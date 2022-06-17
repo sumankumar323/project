b7959b4c6e81cfee13b52a98920e505243e96782
SAKET KUMAR <saketk89@gmail.com>
2022-06-17 15:57:58 +0530

    done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -75,23 +75,7 @@ const deleteUser = async function(req, res) {
 
 
 const postMessage = async function (req, res) {
-    // Check if the token is present
-    // Check if the token present is a valid token
-    // Return a different error message in both these cases
-// let token = req.headers["x-auth-token"]
-//     if(!token) return res.send({status: false, msg: "token must be present in the request header"})
-//     let decodedToken = jwt.verify(token, 'functionup-radon')
-
-//     if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
-    
-//     //userId for which the request is made. In this case message to be posted.
-//     let userToBeModified = req.params.userId
-//     //userId for the logged-in user
-//     let userLoggedIn = decodedToken.userId
-
-//     //userId comparision to check if the logged-in user is requesting for their own data
-    // if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
-
+ 
     let user = await userModel.findById(req.params.userId)
     if(!user) return res.send({status: false, msg: 'No such user exists'})
     
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -12,7 +12,7 @@ const authenticate   = function(req, res, next) {
    console.log(token);
 
     let decodedToken = jwt.verify(token, "functionup-radon");
-    // if (!decodedToken) return res.send({ status: false, msg: "token is invalid" });
+    // if (!decodedToken) return res.send({ status: false, msg: "tokn is ienvalid" });
    }catch(error){
        res.status(401).send({msg: false, error:error.message})  
    }
@@ -22,11 +22,10 @@ const authenticate   = function(req, res, next) {
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
+    try{
     let token = req.headers["x-auth-token"]
     if(!token) return res.status(400).send({status: false, msg: "token must be present in the request header"})
    
-   try{
-
     let decodedToken = jwt.verify(token, 'functionup-radon')
     console.log(decodedToken)
 
