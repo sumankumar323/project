c6caa450282bd97fc4586e8dab8349bd47837d67
sumankumar323 <sangamsuman323@gmail.com>
2022-07-07 13:54:15 +0530

    update

--- src/controllers/reviewcontrollers.js
+++ src/controllers/reviewcontrollers.js
@@ -119,19 +119,19 @@ exports.addReview = async (req,res) =>{
 
 //........................**********------------ DELETE books by reviewsId------*******............ 
  
-exports.deleteBooksByreviewId = async (req,res) => {
-    try {
-      let reviewId = req.params.reviewId;
-      let findreviewId = await reviewmodel.findById({ bookId: _id }) //checking that bookid present inside database or not
-      if (!findreviewId) return res.status(404).send({ status: false, msg: "No reviewId Found" });
-      await reviewmodel.findOneAndUpdate(
-        {_id:  bookId, isDeleted: false },
-        { new: true }
-      );
-      res.status(200).send({ status: true, data: "book is deleted" });
+// exports.deleteBooksByreviewId = async (req,res) => {
+//     try {
+//       let reviewId = req.params.reviewId;
+//       let findreviewId = await reviewmodel.findById({ bookId: _id }) //checking that bookid present inside database or not
+//       if (!findreviewId) return res.status(404).send({ status: false, msg: "No reviewId Found" });
+//       await reviewmodel.findOneAndUpdate(
+//         {_id:  bookId, isDeleted: false },
+//         { new: true }
+//       );
+//       res.status(200).send({ status: true, data: "book is deleted" });
       
-    } catch (error) {
-        res.status(500).send({ status: false, message: error.message })
-    }
-}
+//     } catch (error) {
+//         res.status(500).send({ status: false, message: error.message })
+//     }
+// }
 
