3fba6c5aba9d53dab8edde64c2f9e1cefdb0d517
Ravi kumar sharma <rvsharma2652@gmail.com>
2022-07-07 13:36:59 +0530

    update review

---
+++ src/controllers/reviewcontrollers.js
@@ -0,0 +1,78 @@
+
+
+const bookModel = require("../Model/bookmodel")
+const reviewModel = require("../Model/reviewmodel")
+const validation = require("../validator/validator")
+const ObjectId = require("mongoose").Types.ObjectId
+
+
+
+
+
+exports.updateReview = async  (req, res) => {
+    try {
+        const bookIdParams = req.params.bookId
+        const reviewIdParams = req.params.reviewId
+        const dataToUpdate = req.body
+        // Validation of Id's from Url
+        if (!ObjectId.isValid(bookIdParams)) return res.status(400).send({ status: false, msg: "Not a valid Book id from url." });
+        if (!ObjectId.isValid(reviewIdParams)) return res.status(400).send({ status: false, msg: "Not a valid Review id from url." });
+
+        // Finding the book by Id in url
+        const findBook = await bookModel.findOne({ _id: bookIdParams, isDeleted: false }).select({ __v: 0 })
+        if (!findBook) return res.status(404).send({ status: false, message: "No Book found" })
+
+        // Finding the review by if in url
+        const findReview = await reviewModel.findOne({ _id: reviewIdParams, isDeleted: false })
+        if (!findReview) return res.status(404).send({ status: false, message: "No Review found" })
+
+        // verifying review id belongs to the book or not
+        const bookIdFromReview = findReview.bookId.toString()
+        if (bookIdParams !== bookIdFromReview) return res.status(400).send({ status: false, messege: "The review you want to update not belongs to the book provide in url." })
+
+        // User input validation
+        const { reviewedBy, rating, review } = dataToUpdate
+        if (!validation.isValidReqBody(dataToUpdate)) return res.status(400).send({ status: false, message: "Please enter details you want to update." })
+
+        //If Reviewed by is present 
+        if (reviewedBy == "") {
+            return res.status(400).send({ status: false, message: "Reviewer's name cannot be empty" })
+        } else if (reviewedBy) {
+            if (!validation.isValid(reviewedBy) || !validation.isValidName(reviewedBy))
+                return res.status(400).send({ status: false, message: "ReviewBy Should Contain only alphabets." })
+        }
+        dataToUpdate['reviewedBy'] = reviewedBy
+
+
+        //If Rating by is present 
+        if (rating == "") {
+            return res.status(400).send({ status: false, message: "Rating cannot be empty" })
+        } else if (rating) {
+            if (((rating < 1) || (rating > 5)) || !validation.isValidRating(rating))
+                return res.status(400).send({ status: false, message: "Rating must be 1 to 5 numerical value" });
+        }
+        dataToUpdate['rating'] = rating
+
+        //If Review by is present 
+        if (review == "") {
+            return res.status(400).send({ status: false, message: "Review cannot be empty" })
+        } else if (review) {
+            if (!validation.isValid(review)) return res.status(400).send({ status: false, message: "Review is invalid." })
+        }
+        dataToUpdate['review'] = review
+
+        // Updating The review details
+        const updatedReview = await reviewModel.findOneAndUpdate({ _id: reviewIdParams }, dataToUpdate, { new: true }).select({ isDeleted: 0, createdAt: 0, updatedAt: 0, __v: 0 })
+
+        // Assigning reviews list with book details
+        const booksWithUpdatedReview = findBook.toObject()
+        booksWithUpdatedReview['reviewsData'] = updatedReview
+
+        res.status(200).send({ status: true, messege: "Review Updated successfully", data: booksWithUpdatedReview })
+
+    } catch (err) {
+        return res.status(500).send({ status: false, messege: err.message })
+    }
+}
+
+
